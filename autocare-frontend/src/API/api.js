28f9333d218a3719fe210928ebe1e6030354c824
narshina <narshina2001@gmail.com>
2026-01-21 10:42:09 +0530

    url changed

--- autocare-frontend/src/API/api.js
+++ autocare-frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const API_URL = "http://localhost:5000";
-const API_URL = "https://autocare-full.onrender.com"
+const API_URL = "https://autocare-k43g.onrender.com"
 // const API_URL = import.meta.env.VITE_API_URL;
 
 
